cc7e596406234d32f2a93739c2307851ad4bf77a
Sameer Bagwan <sameerbagwan132@gmail.com>
2024-04-02 19:10:59 +0530

    server setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,17 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
-
+import 'module-alias/register';
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3030;
 
 app.get('/', (req: Request, res: Response) => {
-  res.json({ message: 'Server is Runing changed' });
+  res.json({ message: 'Server is Running' });
+});
+
+app.get('/health', (req: Request, res: Response) => {
+  res.json({ message: 'health is okay' });
 });
 
 app.listen(port, () => {
