2173a9bc08a3566822edc11f53d885aa9f377011
Sameer Bagwan <sameerbagwan132@gmail.com>
2024-04-04 20:08:51 +0530

    client

---
+++ client/src/app/rootReducer.ts
@@ -0,0 +1,6 @@
+import {combineReducers} from '@reduxjs/toolkit';
+import counterReducer from '@/app/features/Counter.slice';
+
+export const rootReducer = combineReducers({
+  counter: counterReducer,
+});
