3ae9044491444f6c481090d9e225e5222aba8bb9
Sameer Bagwan <sameerbagwan132@gmail.com>
2024-04-18 21:37:07 +0530

    properties model

---
+++ server/src/models/properties.model.ts
@@ -0,0 +1,63 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+interface IProperties extends Document {
+    title: string;
+    description: string;
+    price: number;
+    location: {
+        address: string;
+        city: string;
+        state: string;
+        country: string;
+        coordinates: {
+            latitude: number;
+            longitude: number;
+        };
+    };
+    bedrooms: number;
+    bathrooms: number;
+    area: number;
+    amenities: string[];
+    thumbnail: string;
+    mainPhoto: string;
+    morePhotos: string[];
+    sku: string; // Stock Keeping Unit
+    developer: {
+        name: string;
+        contact: string;
+    };
+    broker: string; // Assigned broker
+}
+
+// Define schema for Property document
+const propertiesSchema: Schema = new Schema({
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    location: {
+        address: { type: String, required: true },
+        city: { type: String, required: true },
+        state: { type: String, required: true },
+        country: { type: String, required: true },
+        coordinates: {
+            latitude: { type: Number, required: true },
+            longitude: { type: Number, required: true },
+        },
+    },
+    bedrooms: { type: Number, required: true },
+    bathrooms: { type: Number, required: true },
+    area: { type: Number, required: true },
+    amenities: { type: [String], required: false },
+    thumbnail: { type: String, required: true },
+    mainPhoto: { type: String, required: true },
+    morePhotos: { type: [String], required: false },
+    sku: { type: String, required: true },
+    developer: {
+        name: { type: String, required: true },
+        contact: { type: String, required: true },
+    },
+    broker: { type: String, required: true },
+});
+
+const Properties = mongoose.model<IProperties>('Property', propertiesSchema);
+export default Properties;
